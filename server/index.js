6b7a908b0ec1f50e1054607256c3b52890a282a3
Sumaya Salad <ssalaad13@gmail.com>
2021-06-11 10:30:15 -0400

    finit

--- server/index.js
+++ server/index.js
@@ -92,5 +92,5 @@ app.get('/monthly', monthlyCtrl.getMonthlyLog )
 
 app.use(express.static(__dirname + '/../build'))
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index,html'))
+  res.sendFile(path.join(__dirname, '../build/index.html'))
 })
